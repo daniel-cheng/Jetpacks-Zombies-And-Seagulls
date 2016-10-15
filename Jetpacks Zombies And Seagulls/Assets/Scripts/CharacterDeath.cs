d97efd9d200d96638a9f42c279ef192ae138a212
Roger <roger@dalkefamily.com>
2016-10-14 23:07:59 -0700

    Character Death and Basic Folder Setup

---
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/CharacterDeath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterDeath : MonoBehaviour
+{
+    public static GameObject character;
+
+    void Awake ()
+    {
+        character = gameObject;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy") || col.gameObject.CompareTag("Environment"))
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Instantiate(Resources.Load("Death Canvas"));
+
+        Destroy(gameObject);
+    }
+}
--- Jetpacks Zombies And Seagulls/Assets/Scripts/MainCharacterMovement.cs
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/MainCharacterMovement.cs
@@ -4,25 +4,21 @@ using System.Collections;
 public class MainCharacterMovement : MonoBehaviour {
 
     Rigidbody myRigidbody;
-
-    // Use this for initialization
+    
     void Awake()
     {
         myRigidbody = GetComponent<Rigidbody>();
     }
-
-    // Use this for initialization
+    
     void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
         if (Input.GetButton("Vertical"))
         {
             float vertAxis = Input.GetAxis("Vertical");
             myRigidbody.AddForce(new Vector3(0,vertAxis*5,0));
         }
-        Debug.Log(myRigidbody);
 	}
 }
