277dccef95106c43baedaa607821199bf2628b38
Roger <roger@dalkefamily.com>
2016-10-15 13:06:15 -0700

    Added pause functionality and sunglasses

---
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+    GameObject pauseUI;
+    //bool isPaused;
+
+    void Awake ()
+    {
+        pauseUI = (GameObject)Instantiate(Resources.Load("Pause UI"));
+        pauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame ()
+    {
+        pauseUI.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 0;
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+
+        pauseUI.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/Resume.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resume : MonoBehaviour
+{
+    //Goes on resume button
+    public void ResumeGame()
+    {
+        MainCharacterMovement.character.GetComponent<Pause>().ResumeGame();
+    }
+}
