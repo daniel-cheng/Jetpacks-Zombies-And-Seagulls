db9db381c088d99387b3d580cfb84bc1d9bf60e1
Grant Hirahara <ghirahar@uci.edu>
2016-10-15 12:03:44 -0700

    Added Respawn and Despawn Enemy Script

--- Jetpacks Zombies And Seagulls/Assets/Scripts/Respawn.cs
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/Respawn.cs
@@ -10,4 +10,14 @@ public class Respawn : MonoBehaviour
         Destroy(MainCharacterMovement.character);
         SpawnCharacter.spawner.SpawnPlayer();
 	}
+    void RespawnEnemies()
+    {
+        SpawnEnemy.enemySpawnerReference.KillEnemies();
+        SpawnEnemy.enemySpawnerReference.Spawn(3);
+    }
+    void Restart()
+    {
+        RespawnPlayer();
+        RespawnEnemies();
+    }
 }
---
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    public GameObject enemy;
+    public int enemyCount = 1;
+    private Vector3 spawnPoint;
+    List<GameObject> enemyList = new List<GameObject>();
+    public static SpawnEnemy enemySpawnerReference;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Spawn(enemyCount);       
+	}
+
+    void Awake()
+    {
+        if(enemySpawnerReference == null)
+        {
+            enemySpawnerReference = this;
+        }
+        else if (enemySpawnerReference != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Spawn(int enemyNumber)
+    {
+        for (int i = 0; i < enemyNumber; ++i)
+        {
+            spawnPoint.x = Random.Range(-25, 25);
+            spawnPoint.z = Random.Range(-25, 25);
+            GameObject tempEnemy = (GameObject)Instantiate(enemy, spawnPoint, Quaternion.identity);
+            enemyList.Add(tempEnemy);
+        }
+    }
+    public void KillEnemies()
+    {
+        foreach(GameObject enemyRef in enemyList)
+        {
+            Destroy(enemyRef);
+        }
+        enemyList.Clear();
+    }
+}
