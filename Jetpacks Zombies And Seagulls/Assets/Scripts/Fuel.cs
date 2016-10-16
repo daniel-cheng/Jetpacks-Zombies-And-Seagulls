434d8e2afb30e2fe7b419be312ec9aa1cc2cfe54
Grant Hirahara <ghirahar@uci.edu>
2016-10-16 14:27:04 -0700

    Added a Fuel Guage

--- Jetpacks Zombies And Seagulls/Assets/Scripts/Fuel.cs
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/Fuel.cs
@@ -6,22 +6,27 @@ public class Fuel : MonoBehaviour {
 
     // Use this for initialization
     private float currentFuel = 0;
-    public GameObject fuelBar;
+    private float maxFuel = 100f;
+    public GameObject fuelBar = null;
 	void Start ()
     {
 	    
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        currentFuel = MainCharacterMovement.character.GetComponent<MainCharacterMovement>().jetPackFuel;
 
-        currentFuel = MainCharacterMovement.character.GetComponent< MainCharacterMovement>().jetPackFuel;
-
-        if (currentFuel < 0 )
+        if (currentFuel < 0)
         {
             currentFuel = 0;
         }
-        GetComponent<Text>().text = "Fuel: " + Mathf.Floor(currentFuel).ToString();
+        //GetComponent<Text>().text = "Fuel: " + Mathf.Floor(currentFuel).ToString();
+        fuelBar.transform.localScale = new Vector3(CalculateFuel(currentFuel), fuelBar.transform.localScale.y, fuelBar.transform.localScale.z);
+    }
+    float CalculateFuel(float currentFuel)
+    {
+        return currentFuel / maxFuel;
     }
 }
