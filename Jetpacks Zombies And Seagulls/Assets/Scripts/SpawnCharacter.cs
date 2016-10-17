6df7dd6207cce1cd21a4fed4c16d1ed72054ec63
Roger <roger@dalkefamily.com>
2016-10-16 18:37:12 -0700

    Fixed some stats not reseting when you exit to Main Menu after death
    timer and camera shake

--- Jetpacks Zombies And Seagulls/Assets/Scripts/SpawnCharacter.cs
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/SpawnCharacter.cs
@@ -24,6 +24,9 @@ public class SpawnCharacter : MonoBehaviour
 
     public void SpawnPlayer()
     {
+        CameraShake.shake_intensity = 0f;
+        Timer.ResetTime();
+
         CharacterDeath.isDead = false;
         character = (GameObject)Instantiate(characterPrefab, transform.position, transform.rotation);
         character.GetComponent<Pause>().ResumeGame();
