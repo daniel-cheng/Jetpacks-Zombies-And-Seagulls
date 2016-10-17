0a36ca78d29fbd2be8b3b0ea6051f63aca768bde
Daniel Cheng <dcheng334@gmail.com>
2016-10-16 19:24:31 -0700

    Removing Metal Gear, fixing changescene camera problem.

--- Jetpacks Zombies And Seagulls/Assets/Scripts/ChangeScene.cs
+++ Jetpacks Zombies And Seagulls/Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,8 @@ public class ChangeScene : MonoBehaviour
 {
     public void SceneChange(string sceneName)
     {
+        CameraShake.shaking = false;
+        CameraShake.shake_intensity = 0f;
         SceneManager.LoadScene(sceneName);
     }
 }
